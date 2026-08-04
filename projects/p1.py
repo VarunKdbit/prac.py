34d48dacd95b54165800ad5cfb14a43caed0f98d
VarunKdbit <varunthavarunktha@gmail.com>
2026-08-04 15:18:00 +0530

    initial commit

---
+++ projects/p1.py
@@ -0,0 +1,25 @@
+print("welcome program begins!!!")
+m1=int(input("enter the number of marks scored in sub 1"))
+m2=int(input("enter the number of marks scored in sub 2"))
+m3=int(input("enter the number of marks scored in sub 3"))
+m4=int(input("enter the number of marks scored in sub 4"))
+m5=int(input("enter the number of marks scored in sub 5"))
+m6=int(input("enter the number of marks scored in sub 6"))
+total=m1+m2+m3+m4+m5+m6
+perc=(total/600)*100
+print("the percentage scored by student is :",perc )
+if(perc>90):
+    print("A+")
+elif(perc>80):
+    print("A")
+elif(perc>75):
+    print("B+")
+elif(perc>60):
+    print("B")
+elif(perc>35):
+    print("PASS")
+else :
+    print("FAIL")
+print("end of program")
+
+
