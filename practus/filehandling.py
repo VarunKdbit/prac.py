1fc1db26de21fb03686a6232572b4e11fda148eb
VarunKdbit <varunthavarunktha@gmail.com>
2026-08-11 22:42:59 +0530

    delete operations

--- practus/filehandling.py
+++ practus/filehandling.py
@@ -8,5 +8,9 @@ f.close()
 with open(r"C:\Users\Varun\OneDrive\Desktop\pracpro\practus\demo.txt","r") as t :
     print(t.read())
 
-
+#append operation
+with open(r"C:\Users\Varun\OneDrive\Desktop\pracpro\practus\demo.txt","a+") as f :
+    data=f.write("\n I like to build LLM and AI agents ")
+    f.seek(0)
+    print(f.read())
 
--- practus/tempCodeRunnerFile.py
+++ practus/tempCodeRunnerFile.py
@@ -1,4 +1,2 @@
-with open(r"C:\Users\Varun\OneDrive\Desktop\pracpro\practus\demo.txt","a+") as f :
-    data=f.write("\n I like to build LLM and AI agents ")
-    f.seek(0)
-    print(f.read())
+import os
+os.remove(r"C:\Users\Varun\OneDrive\Desktop\pracpro\practus\sample.txt")
