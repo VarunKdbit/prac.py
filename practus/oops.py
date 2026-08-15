ec61205eb5638a5c34014af28a5c943e81f9016a
VarunKdbit <varunthavarunktha@gmail.com>
2026-08-15 14:06:32 +0530

    decorator

--- practus/oops.py
+++ practus/oops.py
@@ -33,4 +33,30 @@ class Student :
 s1 =Student("varun",[6.7,5.9,6.0,8.4])
 s1.avg()
 
-#program 3
+#program 3 (with using decorator )
+
+class Student :
+    
+    def __init__(self,name,marks) :
+        self.name = name 
+        self.marks =marks
+
+
+    @staticmethod
+    def hello() :
+        print("hello")
+
+    def avg(self) :
+        sum=0
+        for i in self.marks :
+            sum+=i
+        avg=sum/4
+        print("Hi",self.name,"your avg marks is :",avg)
+
+
+s1 =Student("varun",[6.7,5.9,6.0,8.4])
+s1.hello()
+s1.avg()
+
+
+
