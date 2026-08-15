e1e58c15e5e9bf29a5d7df38603bad4dca865c07
VarunKdbit <varunthavarunktha@gmail.com>
2026-08-15 13:22:49 +0530

    methods

--- practus/oops.py
+++ practus/oops.py
@@ -15,3 +15,22 @@ class Student :
 s1 = Student("varun",67)
 s1.welcome()
 print(s1.get_marks())
+
+#program 2
+class Student :
+    
+    def __init__(self,name,marks) :
+        self.name = name 
+        self.marks =marks
+
+    def avg(self) :
+        sum=0
+        for i in self.marks :
+            sum+=i
+        avg=sum/4
+        print("Hi",self.name,"your avg marks is :",avg)
+
+s1 =Student("varun",[6.7,5.9,6.0,8.4])
+s1.avg()
+
+#program 3
