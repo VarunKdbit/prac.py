1d1064436e51aee2c3565d80d8fb2678c09a1494
VarunKdbit <varunthavarunktha@gmail.com>
2026-08-17 22:30:28 +0530

    updation

--- practus/oops.py
+++ practus/oops.py
@@ -244,3 +244,33 @@ class Employee :
 emp1 = Employee("Software Developer","AIML",80000)
 emp1.show_details()
 
+# practise problem 3
+#inheritance pp
+class Employee :
+
+    def __init__(self,role,department,salary) :
+        self.role = role
+        self.department = department 
+        self.salary = salary
+        
+    def show_details(self) :
+        print("ROLE :",self.role)
+        print("DEPARTMENT :",self.department)
+        print("SALARY :",self.salary)
+
+class Engineer(Employee) :
+
+    def __init__(self,name,age,role,dept,salary) :
+        self.rol =role
+        self.dept =dept
+        self.slry =salary
+        self.name = name
+        self.age = age
+        super().__init__(self.rol,self.dept,self.slry)
+
+emp1 = Engineer("VARUN K",21,"PYTHON BACKEND","AIML","3700000")
+emp1.show_details()
+
+
+
+
