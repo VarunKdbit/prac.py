3fe26eeb466a27b0a0db6c2d7f69857569df6df5
VarunKdbit <varunthavarunktha@gmail.com>
2026-08-17 22:27:23 +0530

    pp3

--- practus/work1.py
+++ practus/work1.py
@@ -10,6 +10,19 @@ class Employee :
         print("DEPARTMENT :",self.department)
         print("SALARY :",self.salary)
 
-emp1 = Employee("Software Developer","AIML",80000)
+class Engineer(Employee) :
+
+    def __init__(self,name,age,role,dept,salary) :
+        self.rol =role
+        self.dept =dept
+        self.slry =salary
+        self.name = name
+        self.age = age
+        super().__init__(self.rol,self.dept,self.slry)
+
+emp1 = Engineer("VARUN K",21,"PYTHON BACKEND","AIML","3700000")
 emp1.show_details()
+
+
+
 
