6902411fe98697162b3301afb9e11f5c5de1d058
VarunKdbit <varunthavarunktha@gmail.com>
2026-08-17 22:08:06 +0530

    updation

--- practus/oops.py
+++ practus/oops.py
@@ -211,3 +211,19 @@ class Result :
 
 s1 = Result(75,70,80,70)
 s1.percentage
+
+# practise problem 1
+class Circle :
+
+    def __init__(self,r) :
+        self.radius = r
+
+    def area(self) :
+        return  3.14*self.radius ** 2
+
+    def perimeter(self) :
+        return 2*3.14*self.radius
+
+c1 = Circle(8)
+print(c1.area())
+print(c1.perimeter())
