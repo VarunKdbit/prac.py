d678dd1a36f280bd74a7867e2f5ee822c8e585e3
VarunKdbit <varunthavarunktha@gmail.com>
2026-08-19 19:47:57 +0530

    armstrong number

--- practus/basicds.py
+++ practus/basicds.py
@@ -19,3 +19,39 @@ if nc == x :
     print("Entered number is palindrome")
 else :
     print("Entered number is not a palindrome")
+
+#How to check whether the number is armstrong or not
+# Method 1:
+n=int(input("Enter the number:"))
+num=n
+count=0
+while num >0:
+    count+=1
+    num=num//10
+na=n
+arm=0
+while na>0 :
+    i=na%10
+    arm+=i**count
+    na=na//10
+if(arm==n) :
+    print("armstrong number")
+else :
+    print("not an armstrong number")
+
+# Method2:
+n=int(input("Enter the number : "))
+count=len(str(n))
+num=n
+arm=0
+while num>0 :
+    i=num%10
+    arm+=i**count
+    num=num//10
+if arm==n :
+    print("Entered number is a Armstrong number")
+else :
+    print("Entered number is not a Armstrong number")
+
+
+
