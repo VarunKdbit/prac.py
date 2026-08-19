db910192273795732d2f0af4912818ce9d9a32c7
VarunKdbit <varunthavarunktha@gmail.com>
2026-08-19 19:18:11 +0530

    palindrome program

--- practus/basicds.py
+++ practus/basicds.py
@@ -6,3 +6,16 @@ while n>0 :
     n=n//10
     rev=rev*10 +i
 print("The reversed version of number is :", rev)
+
+#Check number is palindrome or not 
+x=int(input("Enter the number : "))
+n=x
+nc=0
+while n>0 : 
+    i=n%10 
+    n=n//10
+    nc=nc*10 + i
+if nc == x :
+    print("Entered number is palindrome")
+else :
+    print("Entered number is not a palindrome")
--- practus/work1.py
+++ practus/work1.py
@@ -1,7 +1,11 @@
-n = int(input("enter the number  : "))
-rev=0
-while n>0 :
-    i=n%10
+x=int(input("Enter the number : "))
+n=x
+nc=0
+while n>0 : 
+    i=n%10 
     n=n//10
-    rev=rev*10 +i
-print(rev)
+    nc=nc*10 + i
+if nc == x :
+    print("Entered number is palindrome")
+else :
+    print("Entered number is not a palindrome")
